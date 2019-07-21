4d03d4f170173c79a929b907565e34ef42bff814
snowmaan <iliasnk@protonmail.com>
2019-07-21 21:05:26 +0300

    Dropdown menu implemented and some design changes

---
+++ js/dropdownMenuToggle.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    var dropdownContent = document.getElementById("dropdown-content");
+    if (dropdownContent.style.display == "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
+
+//TODO ADD function to dissapear menu when link is clicked
