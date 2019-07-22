cb26e54d1a6e6e5bb6a447fd1a0591ee32956eac
snowmaan <iliasnk@protonmail.com>
2019-07-23 01:10:20 +0300

    Deleted unnecessary js script

--- js/setHeightMain.js
+++
@@ -1,4 +0,0 @@
-function setHeightMain() {
-    var myHeight = window.innerHeight - 50; //50 pixels is the fixed height of the header
-    document.getElementById("main").style.height = myHeight + "px"; //Header and main part of page takes the whole screen
-}
