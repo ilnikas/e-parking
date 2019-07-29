17031fbc8f81a4da55ee4fe95dfe75c355dcc68f
snowmaan <iliasnk@protonmail.com>
2019-07-30 02:15:56 +0300

    Disabled map zooming via wheel scroll

--- js/adminMap.js
+++ js/adminMap.js
@@ -1,10 +1,9 @@
 //coordinates which the map will initially display  --Currently set to Patras,Greece
 const myLong = 38.246208;   
 const myLang = 21.735069;
-const zoomBy = 13; //How much to zoom into the map initially 
+const zoomBy = 14; //How much to zoom into the map initially 
 
-var mymap = L.map('map').setView([myLong, myLang], zoomBy); //Initializing map
-
+var mymap = L.map('map',{ scrollWheelZoom : false }).setView([myLong, myLang], zoomBy); //Initializing map
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors'; //copyright to open street maps
 
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
