d25e385dd1aec633112eba3df446246c2912586a
snowmaan <iliasnk@protonmail.com>
2019-07-31 15:52:46 +0300

    Added on-click marker and radius form on user map

--- js/userMap.js
+++ js/userMap.js
@@ -11,3 +11,24 @@ const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 const tiles = L.tileLayer(tileUrl, { attribution }); //creating tiles for the map
 
 tiles.addTo(mymap);
+
+//Adding marker
+var myMarker = {};
+//What will be displayed on the popup
+var popupContent = "<form action='kati.php'> <input placeholder='Ακτίνα (μέτρα)' style='height: 20px;' type='number' min='1' max='2500' name='radius' required> <br> <div style='margin-top: 12px; text-align: center;'><input style='height: 21px; font-size: 12px; border-radius: 5px; border: 1px solid coral; background-corol: lightgrey;' type='submit' value='Αποστολή'></div> </form>";  
+
+mymap.on("click",function(e) {
+    lat = e.latlng.lat;
+    lon = e.latlng.lng;
+
+    //removing existing marker if it exists
+    if (myMarker != undefined) {
+        mymap.removeLayer(myMarker);
+    }
+
+    //adding the marker
+    myMarker = L.marker([lat,lon]);
+    myMarker.addTo(mymap);
+    myMarker.bindPopup(popupContent).openPopup(); //Adding popup to enter variable "radius" that will be sent to server
+});
+
