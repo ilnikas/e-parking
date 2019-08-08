9616db9b8004d25d615b06ff2ffe61c17870f85f
snowmaan <iliasnk@protonmail.com>
2019-08-08 19:19:45 +0300

    Added comment explanation for login file

--- Backend/phpIncludes/credentials.php
+++ Backend/phpIncludes/credentials.php
@@ -1,4 +1,4 @@
 <?php
 $username = password_hash("HIDDEN",PASSWORD_BCRYPT); //No need to display username so this is hashed as well -- REPLACE HIDDEN WITH DESIRED VALUE TO CHANGE USERNAME
-$password = password_hash("HIDDEN",PASSWORD_BCRYPT); //replace first arguement with your password -- REPLACE HIDDEN WITH DESIRED VALUE TO CHANGE USERNAME
+$password = password_hash("HIDDEN",PASSWORD_BCRYPT); //REPLACE HIDDEN WITH DESIRED VALUE TO CHANGE PASSWORD
 ?>
