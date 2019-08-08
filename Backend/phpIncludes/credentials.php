3e954493eee48d24c1e66180d7dae13f0f607d0e
snowmaan <iliasnk@protonmail.com>
2019-08-08 19:16:05 +0300

    Fixed security mistake in login

--- Backend/phpIncludes/credentials.php
+++ Backend/phpIncludes/credentials.php
@@ -1,14 +1,4 @@
 <?php
-
-
-//______________CHANGE THE VALUE OF VARIABLES TO CHANGE USERNAME AND/OR PASSWORD_________________
-$adminUsername = "HIDDEN";
-$adminPassword = "HIDDEN";
-//______________----------------------------------------------------------------_________________
-
-
-
-
-$username = password_hash($adminUsername,PASSWORD_BCRYPT); //No need to display username so this is hashed as well
-$password = password_hash($adminPassword,PASSWORD_BCRYPT); //replace first arguement with your password
+$username = password_hash("HIDDEN",PASSWORD_BCRYPT); //No need to display username so this is hashed as well -- REPLACE HIDDEN WITH DESIRED VALUE TO CHANGE USERNAME
+$password = password_hash("HIDDEN",PASSWORD_BCRYPT); //replace first arguement with your password -- REPLACE HIDDEN WITH DESIRED VALUE TO CHANGE USERNAME
 ?>
