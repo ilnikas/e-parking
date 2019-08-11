c537bc71827c2194af2e7453b53fc84bcebd7263
tasosxynos <tasosxynos@gmail.com>
2019-08-11 15:42:00 +0300

    Added formated current simulation time variable

--- Frontend/js/startSimulation.js
+++ Frontend/js/startSimulation.js
@@ -1,7 +1,9 @@
 var exactTime;
-var exactHours; //to be returned
-var exactMins; //to be returned
-var offsetTime; //to be returned
+var exactHours;
+var exactMins;
+var absoluteTime;
+var currSimulationTime; //to be returned
+var offsetTime;
 var today = new Date();
 
 var modal2 = document.getElementById("simulationModal");
@@ -11,7 +13,11 @@ var btn3 = document.getElementById("simulationButtonR");
 var closeButton = document.getElementsByClassName("closeModal")[1]; //button that closes poppup
 
 btn1.onclick = function() {
-	//some AJAX to send data time to server
+	absoluteTime = Number(absoluteTime) - Number(offsetTime);
+	if(absoluteTime < 0){ absoluteTime = absoluteTime + Number(1440);}
+	currSimulationTime = absToCurr(absoluteTime);
+	alert("curr simul time is: " +currSimulationTime);
+	//some AJAX to send "currSimulationTime" variable to server
   }
 
 btn2.onclick = function() {
@@ -19,7 +25,10 @@ btn2.onclick = function() {
   }
 
 btn3.onclick = function() {
-    //some AJAX to send data time to server
+    absoluteTime = Number(absoluteTime) + Number(offsetTime);
+	if(absoluteTime > 1439){ absoluteTime = absoluteTime - Number(1440);}
+	currSimulationTime = absToCurr(absoluteTime);
+	alert("curr simul time is: " +currSimulationTime);
   }
 
 closeButton.onclick = function() {
@@ -28,7 +37,18 @@ closeButton.onclick = function() {
 
 exactHours = today.getHours();
 exactMins = today.getMinutes();
-offsetTime = 10;
+
+function absToCurr(absTime){
+	var currSimTime;
+	var exHours = ~~(absTime / 60);
+	var exMins = absTime % 60;
+		if( (exHours < 10) && (exMins < 10) ){		
+		currSimTime = "0" + exHours + "0" + exMins;}
+		else if( exHours < 10 ) {currSimTime = "0" + exHours + exMins;}
+		else if( exMins < 10 ) {currSimTime = "" + exHours + "0" + exMins;}
+		else {currSimTime = "" + exHours + exMins;}
+	return currSimTime;
+}
 
 function setValues(){
 document.getElementById("simulationButtonL").innerHTML = "Εξομοίωση για " +offsetTime +" λεπτά πριν";
@@ -60,9 +80,16 @@ function validate_simul()
   if( emptySet1 == true && emptySet2 == true ) {
 		exactHours = today.getHours();
 		exactMins = today.getMinutes();
-		offsetTime = 10;
+		offsetTime = 15;
+		setValues();
+		leftButton.style = "display: block;";
+		rightButton.style = "display: block;";
+		absoluteTime = exactHours * 60 + Number(exactMins);
+		currSimulationTime = absToCurr(absoluteTime);
+		alert("curr simul time is: " +currSimulationTime);
+		modal2.style.display = "none";
 		return false;
-		//some AJAX to send data time to server
+		//some AJAX to send "currSimulationTime" variable to server
 		}
 
   if( (afterTimeResult == false && emptySet2 == false) && (emptySet1 == true || inputTimeResult ==true) ){
@@ -76,9 +103,12 @@ function validate_simul()
 		setValues();
 		leftButton.style = "display: block;";
 		rightButton.style = "display: block;";
-		alert("egine " +offsetTime);
+		absoluteTime = exactHours * 60 + Number(exactMins);
+		currSimulationTime = absToCurr(absoluteTime);
+		alert("curr simul time is: " +currSimulationTime);
+		modal2.style.display = "none";
 		return false;
-		//some AJAX to send data time to server
+		//some AJAX to send "currSimulationTime" variable to server
 		}
 
   if( inputTimeResult == false && (emptySet2 == true || afterTimeResult ==true) ){
@@ -89,9 +119,16 @@ function validate_simul()
 		exactTime = inputTime.split(/[:.]+/);
 		exactHours = exactTime[0];
 		exactMins = exactTime[1];
-		offsetTime = 10;
+		offsetTime = 15;
+		setValues();
+		leftButton.style = "display: block;";
+		rightButton.style = "display: block;";
+		absoluteTime = exactHours * 60 + Number(exactMins);
+		currSimulationTime = absToCurr(absoluteTime);
+		alert("curr simul time is: " +currSimulationTime);
+		modal2.style.display = "none";
 		return false;
-		//some AJAX to send data time to server
+		//some AJAX to send "currSimulationTime" variable to server
 		}
 
   else if( inputTimeResult == true && afterTimeResult == true ){
@@ -99,10 +136,15 @@ function validate_simul()
 		exactHours = exactTime[0];
 		exactMins = exactTime[1];
 		offsetTime = afterTime;
+		setValues();
 		leftButton.style = "display: block;";
 		rightButton.style = "display: block;";
+		absoluteTime = exactHours * 60 + Number(exactMins);
+		currSimulationTime = absToCurr(absoluteTime);
+		alert("curr simul time is: " +currSimulationTime);
+		modal2.style.display = "none";
 		return false;
-		//some AJAX to send data time to server
+		//some AJAX to send "currSimulationTime" variable to server
 		}
 
   else {
