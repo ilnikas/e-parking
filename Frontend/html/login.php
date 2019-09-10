6cc8a9593eecf52309bd46457ac1c3d61adbc528
tasosxynos <tasosxynos@gmail.com>
2019-09-10 17:46:07 +0300

    Added functionality to admin's popups and fixed minor visual bugs

--- Frontend/html/login.php
+++ Frontend/html/login.php
@@ -45,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <input class="inputField" id="lastInput" type="password" name="password" placeholder="Password">
                         <?php
                           if(isset($wrong)) {
-                            echo "<p style=text-align: center;";
+                            echo "<p style='text-align: center;'";
                             echo $wrong;
                             echo "</p>";
                           }
